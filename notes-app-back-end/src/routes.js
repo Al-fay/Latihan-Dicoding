dc3f54694fb1ce56ac09af0b78c7ce388b005dc9
Al-fay <diva3577@gmail.com>
2025-08-04 14:49:31 +0700

    pembaruan

--- notes-app-back-end/src/routes.js
+++ notes-app-back-end/src/routes.js
@@ -1,10 +1,36 @@
-const addNoteHandler = require("./handler.js");
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNotesByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
+} = require("./handler.js");
 
 const routes = [
   {
     method: "POST",
     path: "/notes",
     handler: addNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNotesByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
   },
 ];
 
